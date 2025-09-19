a7ac530345718f071606e2a9b923ca47d36bcbd6
Yucky Hito <yuckychong@gmail.com>
2025-09-20 02:45:23 +0800

    Implement parse_email_address

--- src/email_address.py
+++ src/email_address.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from src.domain import Domain
+from domain import Domain, parse_domain
 
 
 @dataclass()
@@ -10,3 +10,18 @@ class EmailAddress:
     username: str
     alias: str
     domain: Domain
+
+def parse_email_address(address: str) -> EmailAddress:
+    """Parse an email address into its components."""
+    if "@" not in address:
+        raise ValueError(f"Invalid email address: {address}")
+
+    username, domain_str = address.split("@", 1)
+    if "+" in username:
+        username, alias = username.split("+", 1)
+    else:
+        alias = ""
+
+    domain = parse_domain("http://" + domain_str)
+
+    return EmailAddress(username=username, alias=alias, domain=domain)
