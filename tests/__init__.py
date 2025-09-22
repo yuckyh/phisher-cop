9cd8a1b028fd1cc2cf6888d053f07dac52d8c152
TemariVirus <kiyonnxperiaz@gmail.com>
2025-09-22 15:08:14 +0800

    Add tests for BK-tree

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
 # These weird comments are to stop ruff from removing the "unused" imports
+from .bktree import TestBKTree, TestLevenshteinDistance  # noqa: F401
 from .dataset import TestDataset  # noqa: F401
 from .document import TestDocument  # noqa: F401
 from .feature_extract import TestFeatureExtract  # noqa: F401
