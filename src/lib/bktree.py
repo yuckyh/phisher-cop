765b62f4d49a802d0cec38732808375e1d4e8d83
TemariVirus <kiyonnxperiaz@gmail.com>
2025-09-22 14:49:32 +0800

    Implement BK-tree

---
+++ src/lib/bktree.py
@@ -0,0 +1,103 @@
+from dataclasses import dataclass
+
+from typing_extensions import Callable, Iterable
+
+
+@dataclass
+class BKTreeNode:
+    label: str
+    children: dict[int, "BKTreeNode"]
+
+
+class BKTree:
+    """A BK-tree for efficiently finding close matches to strings."""
+
+    root: BKTreeNode | None = None
+    distance_fn: Callable[[str, str], int]
+
+    def __init__(
+        self,
+        distance_fn: Callable[[str, str], int],
+        items: Iterable[str],
+    ):
+        self.distance_fn = distance_fn
+        for s in items:
+            self.insert(s)
+
+    def insert(self, item: str):
+        """Insert `item` into the tree."""
+        if self.root is None:
+            self.root = BKTreeNode(label=item, children={})
+            return
+
+        parent = self.root
+        while True:
+            distance = self.distance_fn(item, parent.label)
+            # All descendants must have the same distance to the parent,
+            child = parent.children.get(distance)
+            # So item must be a descendant of child
+            if child is not None:
+                parent = child
+                continue
+
+            # No child with this distance, so we insert the item and finish
+            parent.children[distance] = BKTreeNode(label=item, children={})
+            return
+
+    def contains_max_distance(self, item: str, max_distance: int) -> bool:
+        """Check if the tree contains an item with distance at most `max_distance` from `item`."""
+        if self.root is None:
+            return False
+
+        # Traverse the tree, starting from the root
+        nodes_to_visit = [self.root]
+        while len(nodes_to_visit) > 0:
+            node = nodes_to_visit.pop()
+            distance = self.distance_fn(item, node.label)
+            if distance <= max_distance:
+                return True
+
+            # Only visit children that could have distance <= max_distance
+            lower_bound = distance - max_distance
+            upper_bound = distance + max_distance
+            nodes_to_visit.extend(
+                child
+                for d, child in node.children.items()
+                if lower_bound <= d <= upper_bound
+            )
+
+        return False
+
+    def __repr__(self) -> str:
+        return f"BKTree(root={self.root},distance_fn={self.distance_fn})"
+
+
+def levenshtein_distance(s1: str, s2: str) -> int:
+    """Compute the Levenshtein distance between two strings.
+    This functions runs in `O(len(s1) * len(s2))` time and `O(min(len(s1), len(s2)))` space."""
+    # Make s2 the shorter string to use less memory
+    if len(s1) < len(s2):
+        s1, s2 = s2, s1
+    assert len(s1) >= len(s2)
+
+    # Optimization for trivial case
+    if len(s2) == 0:
+        return len(s1)
+
+    # First row of distance matrix is the distances from s1[:0] to s2's prefixes
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        # First element is the distance from s1[:i+1] to s2[:0]
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            # Levenshtein distance considers the operations insertion, deletion, and substitution
+            insertion = previous_row[j + 1] + 1
+            deletion = current_row[j] + 1
+            substitution = previous_row[j] + (c1 != c2)
+            # This is the Levenshtein distance for s1[:i+1] and s2[:j+1]
+            current_row.append(min(insertion, deletion, substitution))
+        # We computed the distances of s1[:i+1] and all prefixes of s2,
+        # so we can move to the next row
+        previous_row = current_row
+
+    return previous_row[-1]
