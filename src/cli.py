d812bab2a8b28c9da57da48102ab8d3dbb499c6c
TemariVirus <kiyonnxperiaz@gmail.com>
2025-09-22 22:51:28 +0800

    Parse CLI flags and arguments

--- src/cli.py
+++ src/cli.py
@@ -1,3 +1,19 @@
 """Entry point for the command line interface."""
 
-raise Exception("TODO: Not implemented")
+import os
+
+import click
+
+
+@click.command()
+@click.argument("filepath", type=click.Path(exists=True, dir_okay=False, readable=True))
+def main(filepath: str):
+    """Reads an email from the file at FILEPATH and prints a confidence score of
+    how likely it is to be a phishing email to stdout, along with relevant stats"""
+    filepath = os.path.realpath(filepath)
+    print(f"{filepath=}")
+    # TODO: implement the rest
+
+
+if __name__ == "__main__":
+    main()
