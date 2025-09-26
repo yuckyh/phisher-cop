d593424abaf62ab0540c1e19b6b80cc6325b4009
TemariVirus <kiyonnxperiaz@gmail.com>
2025-09-26 18:22:44 +0800

    Use absolute imports instead of relative imports

--- tests/bktree.py
+++ tests/bktree.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.lib.bktree import BKTree, levenshtein_distance
+from lib.bktree import BKTree, levenshtein_distance
 
 
 class TestLevenshteinDistance(unittest.TestCase):
