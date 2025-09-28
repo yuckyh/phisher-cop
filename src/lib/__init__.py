94ee5c4ff5333e261cf38d96e4fd7cd894d4665f
Yucky Hito <yuckychong@gmail.com>
2025-09-29 04:19:18 +0800

    Add parallelization utility for improved performance in processing

--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -3,10 +3,14 @@
 import os
 from pathlib import Path
 
+from joblib import Parallel, delayed
 from lib.model import Model
 
 PROJECT_ROOT = Path(os.path.realpath(__file__)).parents[2]
 MODEL_PATH = os.path.join(PROJECT_ROOT, "model.joblib")
+
+def parallelize(func, X):
+    return Parallel(n_jobs=-1)(delayed(func)(x) for x in X)
 
 
 class PhisherCop:
