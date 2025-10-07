a49e0ccf64194685bab7d5ab77326432e8d98fcf
TemariVirus <kiyonnxperiaz@gmail.com>
2025-10-07 17:44:32 +0800

    Add feature_data tests

---
+++ tests/feature_data.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+from tempfile import TemporaryDirectory
+
+from src.lib.feature_data import load_lines_as_set
+
+
+class TestFeatureData(unittest.TestCase):
+    def test_load_lines_as_set(self):
+        with TemporaryDirectory() as tmpdir:
+            filepath = os.path.join(tmpdir, "test.txt")
+            with open(filepath, "w") as f:
+                f.write(
+                    "\n".join(
+                        [
+                            "Hello",
+                            "world",
+                            "",
+                            "hello",
+                            "",
+                        ]
+                    )
+                )
+            actual = load_lines_as_set(filepath, lower=True)
+        expected = {"hello", "world"}
+        self.assertEqual(actual, expected)
