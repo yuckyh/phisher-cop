83e515e9e8f76df797c4f3ca2b73d86f7d379d0d
TemariVirus <kiyonnxperiaz@gmail.com>
2025-10-07 15:59:01 +0800

    Test parallelize

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,18 @@
 # These weird comments are to stop ruff from removing the "unused" imports
+import unittest
+
+from src.lib import parallelize
+
 from .bktree import TestBKTree, TestLevenshteinDistance  # noqa: F401
 from .dataset import TestDataset  # noqa: F401
 from .email import TestDocument  # noqa: F401
 from .feature_extract import TestFeatureExtract  # noqa: F401
+
+
+class TestParallelize(unittest.TestCase):
+    def test_parallelize(self):
+        addTwo = lambda x: x + 2
+        input = [1, 2, 3, 4, 5]
+        expected = list(map(addTwo, input))
+        actual = parallelize(addTwo, input)
+        self.assertEqual(expected, actual)
