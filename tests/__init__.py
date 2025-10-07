dc8ad26a6a67ae50734fcc0c8231251878b83571
TemariVirus <kiyonnxperiaz@gmail.com>
2025-10-07 16:20:00 +0800

    Test Domain

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,11 @@
-# These weird comments are to stop ruff from removing the "unused" imports
 import unittest
 
 from src.lib import parallelize
 
+# These weird comments are to stop ruff from removing the "unused" imports
 from .bktree import TestBKTree, TestLevenshteinDistance  # noqa: F401
 from .dataset import TestDataset  # noqa: F401
+from .domain import TestDomain  # noqa: F401
 from .email import TestDocument  # noqa: F401
 from .feature_extract import TestFeatureExtract  # noqa: F401
 
